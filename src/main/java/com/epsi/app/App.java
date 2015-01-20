3e157e7be8276211af256d98ed6a5b0776c8ae77
unknown <olivier.kessler@gmx.com>
2015-01-20 11:11:30 +0100

    restart

---
+++ src/main/java/com/epsi/app/App.java
@@ -0,0 +1,15 @@
+package com.epsi.app;
+
+/**
+ * Hello world!
+ * Coucou
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );//Test
+        AppTest app = new AppTest();
+        testApp();
+    }
+}
